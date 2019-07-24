79df3673067487c0e1ad22f168a92bb98fa5c8e2
Apribhibhot <aprianto.sudibyo@gmail.com>
2019-07-25 02:49:04 +0700

    test

--- src/main/java/com/apri/test/entity/MataKuliah.java
+++ src/main/java/com/apri/test/entity/MataKuliah.java
@@ -5,10 +5,47 @@
  */
 package com.apri.test.entity;
 
-/**
- *
- * @author Adjie
- */
+import javax.persistence.Column;
+
 public class MataKuliah {
-    
+
+    protected int id;
+
+    @Column(name = "mata_kuliah")
+    private String mataKuliah;
+    private int sks;
+
+    public MataKuliah() {
+
+    }
+
+    public MataKuliah(int id, String mataKuliah, int sks) {
+        this.id = id;
+        this.mataKuliah = mataKuliah;
+        this.sks = sks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMataKuliah() {
+        return mataKuliah;
+    }
+
+    public void setMataKuliah(String mataKuliah) {
+        this.mataKuliah = mataKuliah;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
 }
